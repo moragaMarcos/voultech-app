3c760fcbec0deb0fc3b27edd17b3adc430be5303
Marcos moraga hermosilla <marcos@valuelist.cl>
2025-03-17 12:38:32 -0300

    se crean filtros y su interseeccion

--- src/app/components/products/data-handler/data-handler.component.ts
+++ src/app/components/products/data-handler/data-handler.component.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDatepickerInputEvent, MatDatepickerModule } from '@angular/material/datepicker';
 
 
 
@@ -9,14 +9,67 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
   selector: 'app-data-handler',
   imports: [MatDatepickerModule, CommonModule, FormsModule],
   templateUrl: './data-handler.component.html',
-  styleUrl: './data-handler.component.scss'
+  styleUrl: './data-handler.component.scss',
+  providers: [DatePipe]
 })
-export class DataHandlerComponent {
-  valorRange:number = 0
-  maxPrice: number = 100;
-  onChangeRangeBtn(target:any){
+export class DataHandlerComponent implements OnInit {
+  
+  @Output() stateFilter:EventEmitter<any> = new EventEmitter
+  @Output() dateFilter:EventEmitter<any> = new EventEmitter
+  @Output() priceFilter:EventEmitter<any> = new EventEmitter
+  @Output() orderBy:EventEmitter<any> = new EventEmitter
+
+  @Input() maxPrice: number = 0;
+  @Input() minPrice: number = 0;
+
+
+  orderByPriceAsc:boolean = true
+  orderByDateAsc:boolean = true
+  maxPriceSlider:number = 0
+  isCompleted:boolean = true
+  isInitial:boolean = true
+  isPending:boolean = true
+  dateObj = {start: '', end:''}
+
+
+  constructor(private datePipe: DatePipe){}
+
+  ngOnInit(){ this.maxPriceSlider = this.maxPrice}
+
+  onSelectedStatus(event:Event):void{
+    const checkbox = event.target as HTMLInputElement;
+    const filterObj = {
+      type: checkbox.name,
+      value: checkbox.checked
+    }
+    this.stateFilter.emit(filterObj)
+  }
+  onSelectedDate(type:string, event: MatDatepickerInputEvent<Date>):void{
+    const transFormDate = this.datePipe.transform(event.value, 'dd/MM/yyyy')
+    if(transFormDate){
+     if (type === 'start') {
+       this.dateObj['start'] = transFormDate
+     } else if (type === 'end') {
+       this.dateObj['end'] = transFormDate;
+     }
+     if(this.dateObj['start'] != '' && this.dateObj['end'] != '') {
+       this.dateFilter.emit(this.dateObj)
+       this.dateObj = {start: '', end:''}
+     }
+   }
+  }
+  onOrderBy(event:Event){
+    const checkbox  = event.target as HTMLInputElement
+    const filterObj = {
+      type: checkbox.name,
+      value: checkbox.checked
+    }
+    this.orderBy.emit(filterObj)
+  }
+  onChangeRangeBtn(target:any):void{
     const rangeElement  = target as HTMLInputElement
-    console.log(rangeElement.value);
     
+    this.maxPriceSlider = Number(rangeElement.value)
+    this.priceFilter.emit(rangeElement.value)
   }
 }
--- src/app/components/products/product-list/product-list.component.ts
+++ src/app/components/products/product-list/product-list.component.ts
@@ -1,33 +1,135 @@
 import { Component, inject } from '@angular/core';
 import { DataHandlerComponent } from "../data-handler/data-handler.component";
-import { Product } from '../../../models/product.model';
+import { Product, ProductState } from '../../../models/product.model';
 import { ProductService } from '../../../services/product.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { GlobalCategoryService } from '../../../services/global-category.service';
 import { MatDialog } from '@angular/material/dialog';
 import { CrudModalComponent } from '../crud-modal/crud-modal.component';
+import { max } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
   imports: [DataHandlerComponent, CommonModule],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
+  providers:[DatePipe]
 })
 export class ProductListComponent {
   products:Product[] = []
+  auxProducts: Product[] = []; 
+  maxPrice: number = 0
+  globalFilter:any = {
+    state:{ inicial:true, completado:true, pendiente:true },
+    date:{start:'',end:''}, 
+    maxPrice: 0, 
+    order: {
+      date: {
+        true: () => {
+        this.products = [...this.auxProducts.sort(
+            (a, b) => new Date(a.creationDate).getTime() - new Date(b.creationDate).getTime()
+          )]
+        },
+        false: () => {
+          this.products = [...this.auxProducts.sort(
+            (a, b) => new Date(b.creationDate).getTime() - new Date(a.creationDate).getTime()
+          )]
+        }
+      },
+      price: {
+        true: () => {
+        this.products = [...this.auxProducts.sort((a, b) => a.price - b.price)];
+        },
+        false: () => {
+        this.products = [...this.auxProducts.sort((a, b) => b.price - a.price)];
+        }
+      }
+    }}
+  originalStates:any = { inicial:true, completado:true, pendiente:true}
   readonly dialog = inject(MatDialog);
 
-  constructor(private productService: ProductService, private globalCategoryService:GlobalCategoryService,private router:Router){}
+  constructor(
+    private productService: ProductService, 
+    private globalCategoryService:GlobalCategoryService,
+    private router:Router,
+    private datePipe: DatePipe
+  ){}
   ngOnInit(): void {
      this.globalCategoryService.category$.subscribe(category=>{
        this.products = this.productService.getProductsByCategory(category)
+       this.maxPrice = Math.max(...this.products.map(product => product.price));
+       this.globalFilter.maxPrice = this.maxPrice
+       this.auxProducts = this.products
      })
   }
   goToDetail(id:number) {
     this.router.navigate([`/products/${id}`]);
   }
+  filterState(event:any){
+    this.originalStates[event.type] = event.value
+    this.globalFilter['state'][event.type] = event.value
+    this.products = this.auxProducts.filter(product => this.originalStates[product.state])
+    this.initFilters()
 
+  }
+  filterDate(event:any){
+    // this.products = this.auxProducts.filter(product=>{
+    //   return this.datePipe.transform(product.creationDate,'dd/MM/yyyy')!  >= event.start && this.datePipe.transform(product.creationDate,'dd/MM/yyyy')! <= event.end
+    // })
+    this.globalFilter['date']['start'] = event.start
+    this.globalFilter['date']['end'] = event.end
+    this.initFilters()
+ 
+  }
+  byOrder(event:any){
+    // const obj:any = {
+    //   date: {
+    //     true: () => {
+    //      this.products = [...this.auxProducts.sort(
+    //         (a, b) => new Date(a.creationDate).getTime() - new Date(b.creationDate).getTime()
+    //       )]
+    //     },
+    //     false: () => {
+    //       this.products = [...this.auxProducts.sort(
+    //         (a, b) => new Date(b.creationDate).getTime() - new Date(a.creationDate).getTime()
+    //       )]
+    //     }
+    //   },
+    //   price: {
+    //     true: () => {
+    //      this.products = [...this.auxProducts.sort((a, b) => a.price - b.price)];
+    //     },
+    //     false: () => {
+    //      this.products = [...this.auxProducts.sort((a, b) => b.price - a.price)];
+    //     }
+    //   }
+    // };
+    this.globalFilter['order'][event.type][event.value]()
+    // obj[event.type][event.value]();
+    this.initFilters()
+    
+  }
+  filterPrice(event:any){
+
+    this.products = this.auxProducts.filter(product=>product.price <= event)
+    this.globalFilter['maxPrice'] = event
+    this.initFilters()
+    
+  }
+initFilters(){
+  if(this.globalFilter.date.start != '' && this.globalFilter.date.end != '' ){
+    this.products = this.auxProducts.filter(product => {
+      const productDate = this.datePipe.transform(product.creationDate, 'dd/MM/yyyy')!;
+      return (
+        productDate >= this.globalFilter.date.start &&
+        productDate <= this.globalFilter.date.end &&
+        this.globalFilter['state'][product.state] && 
+        product.price <= this.globalFilter['maxPrice']
+      );
+    });
+  }
+}
   openDialog(isAdd:boolean): void {
     const dialogRef = this.dialog.open(CrudModalComponent, {
       data: {add:isAdd},
